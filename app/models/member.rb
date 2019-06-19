3e704bbdf78d6c3550ca040a9a697fde9139c69f
Takaki <takaki55730317@gmail.com>
2019-06-19 15:58:35 +0900

    Update member.rb

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,2 +1,17 @@
 class Member < ApplicationRecord
+  has_secure_password
+  
+  has_many :entries, dependent: :destroy
+
+  validates :name, presence: true,
+    format: { with: /\A[A-Za-z0-9]*\z/, allow_blank: true,
+              allow_blank: true,
+              message: :invalid_member_name },
+    length: { minimum: 2, maximum: 20, allow_blank: true },
+    uniqueness: { case_sensitive: false }
+  validates :full_name, presence: true, length: { maximum: 20 }
+  validates :email, email: { allow_blank: true }
+
+  attr_accessor :current_password
+  validates :password, presence: { if: :current_password }
 end
