5518c42971d88d180418377ce6fa31fdf50ef734
Takaki <takaki55730317@gmail.com>
2019-06-19 17:22:40 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     resources :entries, only: [:index]
   end
 
+  resources :session, only: [:create, :destroy]
+
   resources :entries do
     resources :images, controller: "entry_images" do
       patch :move_higher, :move_lower, on: :member
