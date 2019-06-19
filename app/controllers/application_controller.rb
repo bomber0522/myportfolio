bf627ae48a0e93cd5bbd7ce6f51f6111eef5e640
Takaki <takaki55730317@gmail.com>
2019-06-19 18:19:03 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_member
+    Member.find_by(id: session[:member_id]) if session[:member_id]
+  end
+  helper_method :current_member
+
+  class LoginRequired < StandardError; end
+  class Forbidden < StandardError; end
+
+  private
+
+  def login__required
+    raise LoginRequired unless current_member
+  end
 end
